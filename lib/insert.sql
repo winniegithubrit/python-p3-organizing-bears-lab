637295cbd4482cdd3042523042d60fe69c79598c
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-05-22 12:40:52 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+-- Inserting bear data
+INSERT INTO bears (name, sex)
+VALUES ('Mr. Chocolate', 'M'),
+       ('Rowdy', 'M'),
+       ('Tabitha', 'F'),
+       ('Sergeant Brown', 'M'),
+       ('Melissa', 'F'),
+       ('Grinch', 'M'),
+       ('Wendy', 'F'),
+       (NULL, 'F');
